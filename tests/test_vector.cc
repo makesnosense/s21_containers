2ed5e2923b7b40b413463ae49b48538547e3ebb6
Alex Berezovskiy <20311393+makesnosense@users.noreply.github.com>
2025-01-13 18:55:04 +0300

    test(vector)

--- tests/test_vector.cc
+++ tests/test_vector.cc
@@ -1,1 +1,28 @@
-//
+#include <gtest/gtest.h>
+
+#include <vector>
+
+#include "s21_containers.h"
+
+template <typename T>
+class VectorTest : public testing::Test {
+ protected:
+  VectorTest() : stl_vec_{65, 66, 67}, s21_vec_{65, 66, 67} {}
+
+  std::vector<T> stl_vec_;
+  s21::vector<T> s21_vec_;
+};
+
+using TestedTypes = ::testing::Types<int, double, char>;
+TYPED_TEST_SUITE(VectorTest, TestedTypes);
+
+// Use TYPED_TEST instead of TEST_F
+TYPED_TEST(VectorTest, SizeIsCorrect) {
+  EXPECT_EQ(this->stl_vec_.size(), this->s21_vec_.size());
+}
+
+TYPED_TEST(VectorTest, ElementsMatch) {
+  EXPECT_EQ(this->stl_vec_[0], this->s21_vec_[0]);
+  EXPECT_EQ(this->stl_vec_[1], this->s21_vec_[1]);
+  EXPECT_EQ(this->stl_vec_[2], this->s21_vec_[2]);
+}
