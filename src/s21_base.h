b127b022dd50274759cc4354157ea14cc20f2356
Alex Berezovskiy <20311393+makesnosense@users.noreply.github.com>
2025-01-13 15:43:59 +0300

    feat: initial structure

---
+++ src/s21_base.h
@@ -0,0 +1,32 @@
+#ifndef S21_BASE_H
+#define S21_BASE_H
+
+namespace s21 {
+
+template <typename T>
+class Iterator {
+ public:
+  using value_type = T;
+  using pointer = T*;
+  using reference = T&;
+  using difference_type = std::ptrdiff_t;
+
+  // Essential iterator operations
+  virtual reference operator*() = 0;
+  virtual iterator& operator++() = 0;    // prefix
+  virtual iterator operator++(int) = 0;  // postfix
+  virtual bool operator==(const iterator& other) const = 0;
+  virtual bool operator!=(const iterator& other) const = 0;
+};
+
+template <typename T>
+struct container_traits {
+  using value_type = T;
+  using size_type = std::size_t;
+  using reference = T&;
+  using const_reference = const T&;
+};
+
+}  // namespace s21
+
+#endif S21_BASE_H
---
+++ src/s21_containers.h
@@ -0,0 +1,4 @@
+#ifndef S21_CONTAINERS_H
+#define S21_CONTAINERS_H
+#include "s21_vector.h"
+#endif  // S21_CONTAINERS_H
