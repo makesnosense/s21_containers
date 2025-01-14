a9caf6e0b46e396beaa1b10fa357d13b98cc1281
nexus192 <88116739+nexus192@users.noreply.github.com>
2025-01-14 20:21:38 +0300

    refactor (base Iterator)

--- src/s21_base.h
+++ src/s21_base.h
@@ -4,22 +4,6 @@
 namespace s21 {
 
 template <typename T>
-class Iterator {
- public:
-  using value_type = T;
-  using pointer = T*;
-  using reference = T&;
-  using difference_type = std::ptrdiff_t;
-
-  // Essential iterator operations
-  virtual reference operator*() = 0;
-  virtual Iterator& operator++() = 0;    // prefix
-  virtual Iterator operator++(int) = 0;  // postfix
-  virtual bool operator==(const Iterator& other) const = 0;
-  virtual bool operator!=(const Iterator& other) const = 0;
-};
-
-template <typename T>
 struct container_traits {
   using value_type = T;
   using size_type = std::size_t;
