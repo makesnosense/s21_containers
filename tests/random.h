f1f4b1438037fb799a81db9e25937f958c025d62
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-01-20 10:53:25 +0300

    feat(tests): added proper random generator for tests

---
+++ tests/random.h
@@ -0,0 +1,30 @@
+#ifndef RANDOM_MT_H
+#define RANDOM_MT_H
+
+#include <array>
+#include <chrono>
+#include <random>
+
+namespace Random {
+
+inline std::mt19937 GenerateSeedSequence() {
+  std::random_device system_entropy{};
+  const auto time_seed = static_cast<std::seed_seq::result_type>(
+      std::chrono::steady_clock::now().time_since_epoch().count());
+
+  std::seed_seq seed_sequence{
+      time_seed,        system_entropy(), system_entropy(), system_entropy(),
+      system_entropy(), system_entropy(), system_entropy(), system_entropy()};
+
+  return std::mt19937{seed_sequence};
+}
+
+inline std::mt19937 mt{GenerateSeedSequence()};
+
+inline int Get(const int min, const int max) {
+  return std::uniform_int_distribution{min, max}(mt);
+}
+
+}  // namespace Random
+
+#endif
