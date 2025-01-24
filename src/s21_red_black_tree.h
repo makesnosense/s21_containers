15725d9379e671eb608bc185bfa2cc410b147371
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-01-24 08:38:40 +0300

    feat(red_black_tree): draft insert and print

--- src/s21_red_black_tree.h
+++ src/s21_red_black_tree.h
@@ -11,39 +11,39 @@ enum class NodeColor : int8_t { RED = 0, BLACK = 1 };
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wpadded"
-template <typename KeyType, typename ValueType>
+template <typename Key, typename T>
 class Node {
  public:
-  std::pair<const KeyType, ValueType> data_;
+  std::pair<const Key, T> data_;
   Node* left_{nullptr};
   Node* right_{nullptr};
   Node* parent_{nullptr};
   NodeColor color_{NodeColor::RED};
 
-  Node(const KeyType& key, const ValueType& value) : data_(key, value) {}
+  Node(const Key& key, const T& value) : data_(key, value) {}
 };
 #pragma GCC diagnostic pop
 
-template <typename KeyType, typename ValueType>
+template <typename Key, typename T>
 class Tree;
 
-template <typename KeyType, typename ValueType>
-void print_tree(const Tree<KeyType, ValueType>& tree);
+template <typename Key, typename T>
+void print_tree(const Tree<Key, T>& tree);
 
-template <typename KeyType, typename ValueType>
+template <typename Key, typename T>
 class Tree {
  public:
-  using node = Node<KeyType, ValueType>;
-  using node_data_type = std::pair<const KeyType, ValueType>;
-  using key_type = KeyType;
-  using mapped_type = ValueType;
+  using node = Node<Key, T>;
+  using value_type = std::pair<const Key, T>;
+  // using key_type = Key;
+  // using mapped_type = T;
   // using value_type = typename traits::value_type;
   // using pointer = T*;
   // using const_pointer = const T*;
   // using reference = typename traits::reference;
   // using const_reference = typename traits::const_reference;
   using size_type = std::size_t;
-  friend void print_tree<KeyType, ValueType>(const Tree<KeyType, ValueType>&);
+  friend void print_tree<Key, T>(const Tree<Key, T>&);
 
   Tree() : root_(nullptr), size_(0) {}
   Tree(const Tree&) = delete;
@@ -51,31 +51,78 @@ class Tree {
   Tree(Tree&& other) = delete;
   Tree& operator=(Tree&& other) = delete;
 
+  // void insert(const Key& key, const T& obj) {
+  //   if (root_ == nullptr) {
+  //     root_ = new node(key, obj);
+  //   }
+  //   if (root_->left_->data_.first < key) {
+  //     insert(key, obj);
+
+  //   }
+  // }
+
+  std::pair<node*, bool> insert(const value_type& value) {
+    if (root_ == nullptr) {
+      root_ = new node(value.first, value.second);
+      root_->color_ = NodeColor::BLACK;
+      ++size_;
+      return {root_, true};
+    } else {
+      node* current = root_;
+      while (true) {
+        if (value.first < current->data_.first) {
+          if (current->left_ == nullptr) {
+            current->left_ = new node(value.first, value.second);
+            current->left_->parent_ = current;
+            ++size_;
+            return {current->left_, true};
+          }
+          current = current->left_;
+        } else if (value.first > current->data_.first) {
+          if (current->right_ == nullptr) {
+            current->right_ = new node(value.first, value.second);
+            current->right_->parent_ = current;
+            ++size_;
+            return {current->right_, true};
+          }
+          current = current->right_;
+        } else {
+          return {current, false};
+        }
+      }
+    }
+  }
+
  private:
   node* root_;
   size_type size_;
 };
-
 template <typename KeyType, typename ValueType>
 void print_tree_helper(const Node<KeyType, ValueType>* root,
-                       std::string prefix = "", bool is_left = false) {
+                       std::string prefix = "", std::string child_prefix = "") {
   if (!root) return;
 
-  std::cout << prefix;
-  std::cout << (is_left ? "├──" : "└──");
+  std::cout << prefix << root->data_.first << "("
+            << (root->color_ == NodeColor::RED ? "R" : "B") << ")\n";
 
-  std::cout << root->data_.first << "("
-            << (root->color_ == NodeColor::RED ? "R" : "B") << ")" << std::endl;
+  if (root->right_) {
+    bool has_left = (root->left_ != nullptr);
+    std::cout << child_prefix << "│\n";
+    std::cout << child_prefix << "├── RIGHT: ";
+    print_tree_helper(root->right_, "",
+                      child_prefix + (has_left ? "│   " : "    "));
+  }
 
-  std::string child_prefix = prefix + (is_left ? "│   " : "    ");
-
-  print_tree_helper(root->left_, child_prefix, true);
-  print_tree_helper(root->right_, child_prefix, false);
+  if (root->left_) {
+    std::cout << child_prefix << "│\n";
+    std::cout << child_prefix << "└── LEFT: ";
+    print_tree_helper(root->left_, "", child_prefix + "    ");
+  }
 }
 
 // Main print function
-template <typename KeyType, typename ValueType>
-void print_tree(const Tree<KeyType, ValueType>& tree) {
+template <typename Key, typename T>
+void print_tree(const Tree<Key, T>& tree) {
   std::cout << "\nTree contents:\n";
   print_tree_helper(tree.root_);
   std::cout << "Size: " << tree.size_ << "\n\n";
