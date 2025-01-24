5de25dfffb64d14732574ceacf85c79e7f8e4655
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-01-24 08:56:02 +0300

    test(red_black_tree): initial dummy test

--- tests/test_red_black_tree.cc
+++ tests/test_red_black_tree.cc
@@ -4,4 +4,18 @@
 #include <gtest/gtest.h>
 #pragma GCC diagnostic pop
 
-TEST(RedBlackTreeTest, Meow) { ; }
+TEST(RedBlackTreeTest, Meow) {
+  s21::Tree<int, int> meow;
+  meow.insert({33, 999});
+
+  meow.insert({65, 999});
+  meow.insert({77, 999});
+  meow.insert({12, 999});
+
+  meow.insert({1, 999});
+  meow.insert({42, 999});
+
+  meow.insert({55, 999});
+  meow.insert({66, 999});
+  print_tree(meow);
+}
