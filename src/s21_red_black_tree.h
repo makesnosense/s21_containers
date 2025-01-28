5ffda4459d48dbd94a613d09150bdc3db21fa875
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-01-28 03:06:12 +0300

    fix(red_black_tree): transplant

--- src/s21_red_black_tree.h
+++ src/s21_red_black_tree.h
@@ -235,6 +235,7 @@ class RedBlackTree {
   void Transplant(node* old_node, node* new_node) {
     if (IsRoot(old_node)) {
       root_ = new_node;
+      new_node->parent_ = nullptr;
       return;
     }
 
