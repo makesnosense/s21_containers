3cce2c4a6c75cf05d1173d446d1c6ac688ad4963
Alex Berezovskiy <makesnosense@users.noreply.github.com>
2025-02-03 17:12:51 +0300

    chore: structure

--- src/s21_containers.h
+++ src/s21_containers.h
@@ -1,10 +1,8 @@
 #ifndef S21_CONTAINERS_H
 #define S21_CONTAINERS_H
 
-#include "s21_array.h"
 #include "s21_list.h"
 #include "s21_map.h"
-#include "s21_multiset.h"
 #include "s21_queue.h"
 #include "s21_set.h"
 #include "s21_stack.h"
